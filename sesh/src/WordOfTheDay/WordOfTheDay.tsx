9b341ac81a42072ed5c2a874185109d87902c049
Nanar Boursalian <nboursalian@lyft.com>
2023-04-10 13:45:12 -0700

    Started word of the day

---
+++ sesh/src/WordOfTheDay/WordOfTheDay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WordOfTheDay() {
+    return (
+        <div>
+            <h1>Word</h1>
+            <p>Word Description ... </p>
+        </div>
+    );
+}
+
+export default WordOfTheDay;
